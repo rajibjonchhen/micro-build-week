4b354dd555e9d3917480c555aa94137f17083806
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-20 09:56:05 +0000

    initiated and linked

--- newspaper/src/App.js
+++ newspaper/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavBar from './components/MyNavBar.jsx'
 import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,7 +8,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <MyNavBar/>
+     <MyCategory/>
+     <MyJumbotron/>
+     <MyPostContainer/>
+     <MyArticles/>
     </div>
   );
 }
