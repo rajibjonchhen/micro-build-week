c9c854b399e4fcf2a0865d3fce12e69008d6cb3e
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-21 09:24:16 +0000

    nav bar added to the app.js

--- newspaper/src/App.js
+++ newspaper/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./MyCssCategory.css";
 import MyCategory from "./components/MyCategory";
+import MyNavBar from "./components/MyNavBar";
 import "./App.css";
 
 export default class App extends React.Component {
@@ -17,6 +18,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <MyNavBar/>
         <MyCategory setSearchQuery={this.setSearchQuery} />
       </div>
     );
