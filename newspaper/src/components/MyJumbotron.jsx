59f59613dec9294fc2973e29011ee1b62609cf08
DonAzi <savaSh-00@mail.ru>
2021-12-21 14:02:37 +0000

    jumbotron+SideBar added

--- newspaper/src/components/MyJumbotron.jsx
+++ newspaper/src/components/MyJumbotron.jsx
@@ -1,25 +1,52 @@
 import React from "react";
 import { Container, Jumbotron } from "react-bootstrap";
+import "./MyJumbotron.css";
+import SideBar from "../components/SideBar";
 
 export default class MyJumbotron extends React.Component {
+  state = {
+    // news: [],
+    object1: {},
+  };
+  componentDidMount = async () => {
+    try {
+      let response = await fetch(
+        "https://newsapi.org/v2/everything?q=politics&apiKey=b05b2456108e40a9af9732b781e51033"
+      );
+      console.log("THIS is the Response", response); //CHECK
+      if (response.ok) {
+        let data = await response.json();
+
+        // this.setState({ news: data.articles }); why it did not work  as an array
+        this.setState({ object1: data.articles[2] });
+        console.log("This is a data", data.articles);
+      } else {
+        console.log("SOMETHING WENT WRONG"); //CHECK
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     return (
       <Container>
-        <Jumbotron className="bg-dark text-light">
-          <div className="">
-            <h1>Title of a longer featured blog post</h1>
-            <p>
-              Multiple lines of text that form the lede, informing new readers
-              quickly and efficiently about what's most interesting in this
-              post's contents.
-            </p>
-            <p>
-              <a href="" className="">
-                Continue reading..
-              </a>
-            </p>
-          </div>
+        <Jumbotron className="p-5 bg-dark text-light">
+          <h1 className="text-white font-italic display-4">
+            {this.state.object1.title}
+          </h1>
+          <p className="lead my-3">{this.state.object1.content}</p>
+          <p>
+            {/* note: ask ubeyt about href links&fetch! */}
+            <a
+              href={`${this.state.object1.url}`}
+              className="font-weight-bolder text-light"
+            >
+              Continue reading..
+            </a>
+          </p>
         </Jumbotron>
+        <SideBar></SideBar>
       </Container>
     );
   }
